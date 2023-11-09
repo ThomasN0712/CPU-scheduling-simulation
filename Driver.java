d5feac91dfb89101263d066ea1af46c443b56b42
ThomasN0712 <92189845+ThomasN0712@users.noreply.github.com>
2023-11-08 21:45:55 -0800

    Update RR.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,3 +1,4 @@
+
 /**
  * Driver.java
  * 
@@ -10,41 +11,38 @@
  * where 
  *  schedule is schedule of tasks
  *
- *  algorithm = [FCFS, SJF, PRI, RR, PRI-RR]
+ *  algorithm = [FCFS, PRI, RR]
  */
-  
+
 import java.util.*;
 import java.io.*;
 
-public class Driver
-{
+public class Driver {
     public static void main(String[] args) throws IOException {
-        // if (args.length != 2) {
-        //     System.err.println("Usage: java Driver <algorithm> <schedule>");
-        //     System.exit(0);
-        // }
+        if (args.length != 2) {
+            System.err.println("Usage: java Driver <algorithm> <schedule>");
+            System.exit(0);
+        }
 
-        // BufferedReader inFile = new BufferedReader(new FileReader(args[1]));
-        BufferedReader inFile = new BufferedReader(new FileReader("schedule.txt"));
+        BufferedReader inFile = new BufferedReader(new FileReader(args[1]));
 
         String schedule;
 
         // create the queue of tasks
         List<Task> queue = new ArrayList<Task>();
 
-        // read in the tasks and populate the ready queue        
-        while ( (schedule = inFile.readLine()) != null) {
+        // read in the tasks and populate the ready queue
+        while ((schedule = inFile.readLine()) != null) {
             String[] params = schedule.split(",\\s*");
             queue.add(new Task(params[0], Integer.parseInt(params[1]), Integer.parseInt(params[2])));
         }
 
         inFile.close();
-        
-        Algorithm scheduler = null;
-        // String choice = args[0].toUpperCase();
-        String choice = "PRI";
 
-        switch(choice) {
+        Algorithm scheduler = null;
+        String choice = args[0].toUpperCase();
+
+        switch (choice) {
             case "FCFS":
                 scheduler = new FCFS(queue);
                 break;
--- RR.java
+++ RR.java
@@ -1,54 +1,52 @@
+
 /**
  * Non-preemptive priority scheduling algorithm using RR.
  *
  * This algorithm will run tasks according to round-robin scheduling.
  */
- 
+
 import java.util.*;
 
 // Your code here
-public class RR implements Algorithm 
-{
+public class RR implements Algorithm {
   private List<Task> queue;
   private int timeQuantum;
 
-
-  //constructor, initializes queue and sets the timeQuantum
+  // constructor, initializes queue and sets the timeQuantum
   public RR(List<Task> queue) {
     this.queue = queue;
     this.timeQuantum = 10;
   }
 
-  //algorithm itself
+  // algorithm itself
   public void schedule() {
     System.out.println("-------------------Round Robin Scheduling---------------------\n");
     while (queue.isEmpty() == false) {
-      //read process from queue
+      // read process from queue
       Task task = pickNextTask();
-      
-      //if task burst time shorter than time quantum, run for task's burst time
-      if(task.getBurst() < timeQuantum){
+
+      // if task burst time shorter than time quantum, run for task's burst time
+      if (task.getBurst() <= timeQuantum) {
         CPU.run(task, task.getBurst());
         task.setBurst(0);
       }
-      //if task burst time larger than time quantum, run for time quantum
+      // if task burst time larger than time quantum, run for time quantum
       else {
         CPU.run(task, timeQuantum);
-        task.setBurst(task.getBurst()-timeQuantum); 
+        task.setBurst(task.getBurst() - timeQuantum);
       }
 
-      //if task is not done, move to the back of the queue
-      if(task.getBurst() > 0) {
+      // if task is not done, move to the back of the queue
+      if (task.getBurst() > 0) {
         queue.remove(task);
         queue.add(task);
       }
-      //if the task is done, remove forever 
-      else{
+      // if the task is done, remove forever
+      else {
+        System.out.println("Task finished: " + task.getName() + "\n");
         queue.remove(task);
       }
-
     }
-
   }
 
   public Task pickNextTask() {
